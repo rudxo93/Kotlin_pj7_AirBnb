c5e61d9baf8fb6cb625dc6a6ec80035f38d3e227
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-05-11 22:20:29 +0900

    OnMapReadyCallback 인터페이스 구현 후 지도 객체 등록 후 naverMap객체 전달과 범위 설정

--- app/src/main/java/com/duran/airbnb/MainActivity.kt
+++ app/src/main/java/com/duran/airbnb/MainActivity.kt
@@ -3,12 +3,16 @@ package com.duran.airbnb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val mapView: MapView by lazy {
         findViewById<MapView>(R.id.mapView)
     }
+
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,19 @@ class MainActivity : AppCompatActivity() {
 
         // onCreate 연결
         mapView.onCreate(savedInstanceState)
+
+        // 지도 객체를 얻어서 등록
+        // mapView의 getMapAsync() 메서드로 OnMapReadyCallback을 등록하면 비동기로 naverMap 객체를 얻어올 수 있다.
+        // naverMap객체가 준비되면 onMapReady 콜백 메서드가 호출된다.
+        mapView.getMapAsync(this)
+    }
+
+    // 지도 객체를 사용할 수 있을 때 해당 함수 자동으로 호출
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 18.0 // 지도의 최대 줌 레벨
+        naverMap.minZoom = 10.0 // 지도의 최소 줌 레벨
     }
 
     override fun onStart() {
